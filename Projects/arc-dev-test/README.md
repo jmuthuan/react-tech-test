f03a55154e1e6b784ff94c5275977e4fb159868a
jose muthuan <josemuthuan@gmail.com>
2023-08-02 15:09:27 -0300

    innerJoin function with Javascript

---
+++ Projects/arc-dev-test/README.md
@@ -0,0 +1,6 @@
+write a function to perform an "inner Join"
+
+the function recives 3 parameters: 
+1- list of Object (directory)
+2- list of object (registrations)
+3- A "key" (an String) related to the field name we want to join
---
+++ Projects/arc-dev-test/test-data.js
@@ -0,0 +1,18 @@
+const directory = [
+    { "name": "Maggie", "age": 14, "id": 0, "phone": "+123456" },
+    { "name": "Joseph", "age": 24, "id": 1, "phone": "+145454" },
+    { "name": "Margaret", "age": 11, "id": 2, "phone": "+125556" },
+    { "name": "Elizabeth", "age": 61, "id": 3, "phone": "+765756" },
+    { "name": "Julio", "age": 24, "id": 4, "phone": "165756" },
+    { "name": "Kevin", "age": 64, "id": 5, "phone": "+888856" },
+    { "name": "Martin", "age": 71, "id": 6, "phone": "12323456" },
+    { "name": "Aaron", "age": 30, "id": 7, "phone": "+12434343" },
+];
+
+const registrations = [
+    { "name": "Maggie", "age": 14, "id": 0, "email": "maggie@notreal.com", "confirmed": true },
+    { "name": "Elizabeth", "age": 61, "id": 1, "email": "elizabeth@notreal.com", "confirmed": false },
+    { "name": "Martin", "age": 71, "id": 2, "email": "artingnotreal.com", "confirmed": false },
+    { "name": "Aaron", "age": 30, "id": 3, "email": "aaronnotreal.com", "confirmed": true },
+]
+export default { directory, registrations };
