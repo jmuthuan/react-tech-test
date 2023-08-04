5f3a1615dae451fd1016642ff5a11df6eeaa6ed2
jose muthuan <josemuthuan@gmail.com>
2023-08-04 14:51:29 -0300

    hand programming test solution

---
+++ Projects/strange-mirror/test.js
@@ -0,0 +1,13 @@
+const translate = require('./handProgramming.js');
+
+test("it translates 'hello' correctly", () => {
+    expect(
+        translate("👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊")
+    ).toBe('Hello')
+})
+
+test("it translates 'Hello World!' correctly", () => {
+    expect(
+        translate("👉👆👆👆👆👆👆👆👆🤜👇👈👆👆👆👆👆👆👆👆👆👉🤛👈👊👉👉👆👉👇🤜👆🤛👆👆👉👆👆👉👆👆👆🤜👉🤜👇👉👆👆👆👈👈👆👆👆👉🤛👈👈🤛👉👇👇👇👇👇👊👉👇👉👆👆👆👊👊👆👆👆👊👉👇👊👈👈👆🤜👉🤜👆👉👆🤛👉👉🤛👈👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👉👊👆👆👆👊👇👇👇👇👇👇👊👇👇👇👇👇👇👇👇👊👉👆👊👉👆👊")
+    ).toBe('Hello World!')
+})
